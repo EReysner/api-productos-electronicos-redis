2c4ba3ec676539167a461b121779e4b674102f6b
Victor <EReysner@hotmail.com>
2026-05-13 12:21:20 -0600

    notas

--- src/main/java/com/example/rediscachecrud/exception/ErrorResponse.java
+++ src/main/java/com/example/rediscachecrud/exception/ErrorResponse.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
 import java.util.Map;
 
+// clase para representar la estructura del error response
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
